0b0ab053fed84743ac2873ef8388058857ff77ad
Elle Barnaby <ebarnaby@srcuk.com>
2024-10-22 11:56:15 +0100

    adding to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import React, { useState } from "react";
+import ProductList from "./ProductList";
+import "./App.css";
+import AboutUs from "./AboutUs";
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
   //when app is loaded, hide product list
 
@@ -15,25 +13,30 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}> 
+      <div className={`landing-page ${showProductList ? "fade-out" : ""}`}>
         {/* if product list is visible, fade out */}
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
+            <button
+              className="get-started-button"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </button>
+          </div>
           <div className="aboutus_container">
-          <AboutUs/>
-          {/*Links to about us page to display component*/}
+            <AboutUs />
+            {/*Links to about us page to display component*/}
           </div>
-          </div>
+        </div>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+      <div
+        className={`product-list-container ${showProductList ? "visible" : ""}`}
+      >
         <ProductList />
         {/*Gets product list array*/}
       </div>
@@ -42,6 +45,3 @@ function App() {
 }
 
 export default App;
-
-
-
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import handleAddedToCart from "./CartItem";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload; //holds data for update
+      const existingItem = state.items.find((item) => item.name === name);
+      //finds the items state, checks if the name is the same
+      if (existingItem) {
+        //if item already exists, it adds to the quantity
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+        //otherwise adds item to array
+      }
+      //adds new item to cart, and adds 1 if already there
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (items) => items.name !== action.payload
+      );
+      //filters the items to see if its already added
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name, quantity} = action.payload; //finds data in payload
+      const itemToUpdate = state.items.find(item => item.name === name); //finds name
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
