360a3bea241199b1cc1a4c5aecb26b61af11901a
Elle Barnaby <ebarnaby@srcuk.com>
2024-10-22 10:50:49 +0100

    display plants array & comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,35 @@ import AboutUs from './AboutUs';
 function App() {
   
   const [showProductList, setShowProductList] = useState(false);
+  //when app is loaded, hide product list
 
   const handleGetStartedClick = () => {
     setShowProductList(true);
-  };
+  }; //when button clicked, show product list
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}> 
+        {/* if product list is visible, fade out */}
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
          <h1>Welcome To Paradise Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
-         
           <button className="get-started-button" onClick={handleGetStartedClick}>
             Get Started
           </button>
          </div>
           <div className="aboutus_container">
           <AboutUs/>
+          {/*Links to about us page to display component*/}
           </div>
           </div>
-
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList />
+        {/*Gets product list array*/}
       </div>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,10 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+
 function ProductList() {
-    const [showCart, setShowCart] = useState(false); 
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showCart, setShowCart] = useState(false); //hides the visitbility of the cart
+    const [showPlants, setShowPlants] = useState(false); // hides the visibility of the About Us page
 
     const plantsArray = [
         {
@@ -212,6 +213,7 @@ function ProductList() {
             ]
         }
     ];
+
    const styleObj={
     backgroundColor: '#4CAF50',
     color: '#fff!important',
@@ -244,8 +246,9 @@ const handlePlantsClick = (e) => {
 
    const handleContinueShopping = (e) => {
     e.preventDefault();
-    setShowCart(false);
+    setShowCart(false); //hides cart
   };
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -262,28 +265,36 @@ const handlePlantsClick = (e) => {
               
             </div>
             <div style={styleObjUl}>
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div> 
+                {/* links to plants*/}
                 <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                {/* links to cart*/}
             </div>
         </div>
-        {!showCart? (
+
+        {!showCart? ( //start of a huge if statement. if !showcart is false then display plants
         <div className="product-grid">
-               {plantsArray.map((category, index) => (
+       {plantsArray.map((category, index) => (
+        // searches through array for the catagory and index
     <div key={index}>
+        {/* index is the key */}
         <h1><div>{category.category}</div></h1>
         <div className="product-list">
             {category.plants.map((plant, plantIndex) => (
+                // searches for the catagory and plants
             <div className="product-card" key={plantIndex}>
                 <img className="product-image" src={plant.image} alt={plant.name} />
                 <div className="product-title">{plant.name}</div>
+                {/*Similarly like the above plant.name show other details like description and cost*/}
                 <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                {/* adds to cart */}
             </div>
             ))}
         </div>
     </div>
-    ))} 
+    ))}
         </div>
- ) :  (
+ ) :  ( //if !showcart is true then continue shopping
     <CartItem onContinueShopping={handleContinueShopping}/>
 )}
     </div>
