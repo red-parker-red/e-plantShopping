763fe03b525be4bb5bcdba4bb0dfabecaabb1fa7
red-parker-red <red.parker.red@gmail.com>
2024-11-15 14:04:16 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./e-plantShopping",
+  base: "./",
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx'], // Ensure Vite resolves .jsx files
-  },
-  build: {
-    outDir: 'dist', // Output directory for the build
-  },
 })
